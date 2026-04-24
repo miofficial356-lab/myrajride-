4c41ce43b530f759e6aa260c86b71bfe802e90a7
miofficial356-lab <miofficial356@gmail.com>
2026-04-24 13:00:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ function toggleMenu() {
   let menu = document.getElementById("menu");
   menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
 }
+window.addEventListener("scroll", function() {
+  let elements = document.querySelectorAll(".fade-in");
+
+  elements.forEach(el => {
+    let position = el.getBoundingClientRect().top;
+    let screenHeight = window.innerHeight;
+
+    if (position < screenHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
